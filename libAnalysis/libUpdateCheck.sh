98c386ea42ab7c833e64dd35bccdc75ef8b6ce25
Dan <dxkvse@rit.edu>
2017-07-30 19:14:15 -0400

    random update 0 not working

--- libAnalysis/libUpdateCheck.sh
+++ libAnalysis/libUpdateCheck.sh
@@ -10,6 +10,58 @@ db="/Users/imagine/Desktop/RCode/data/database-Win.sqlite"
 clear;
 
 
+#myvar=$(sqlite3 $db "select * from view_processedLibrarysauthors limit 5;")
+
+
+
+results="$(sqlite3 $db 'select dcl_id, action, dependencies from view_processedLibrarysauthors limit 5')"
+
+#for rows in "${results[@]}"
+#do
+#  fieldA=`echo ${rows}| awk '{print $1}'`;
+#  fieldB=`echo ${rows}| awk '{print $2}'`;
+#  fieldC=`echo ${rows}| awk '{print $3}'`;
+
+#echo ${rows}| awk '{print $3}' 
+
+
+#  echo ${fieldA} ${fieldB} ${fieldC};
+#done
+
+
+
+#cnt=${#results[@]}
+#for (( i=0 ; i<cnt ; i++ ))
+#do
+#    echo ${results[$i]}
+  #  fieldA=${results[0]};
+  #  fieldB=${results[1]};
+  #  fieldC=${results[2]};
+  #  echo $fieldA
+#done
+echo ${results[0]}
+
+#while read a b c
+#do
+#	echo "Dan"
+ #   echo "..${a}..${b}..${c}.."
+#done < <(echo "select dcl_id, action, dependencies from view_processedLibrarysauthors limit 5" | sqlite3 $db)
+
+
+
+#while read a b c
+#do
+#    echo "..${a}..${b}..${c}.."
+#done < <(echo "SELECT A, B, C FROM table_a" | mysql database -u $user -p $password)
+
+
+#echo "dan"
+
+
+
+exit
+
+
 ### Remove all numbers from String
 ### Eliminate all version info
 function cleanString {
@@ -18,7 +70,7 @@ function cleanString {
 	strVal=${strVal//./}  # Remove .
 	strVal=${strVal//:/}  # Remove :
 	strVal=${strVal//@/}  # Remove @
-	strVal=${strVal//[0-9]/}
+	strVal=${strVal//[0-9]/} # Remove all numbers
 
 	echo $strVal
 }
@@ -32,24 +84,8 @@ function updateTypeStringMatch {
 	str_orig=`cleanString $1`
 	str_update=`cleanString $2`
 
-
 	#echo $str_orig
 	#echo $str_update
-
-
-#	str_orig=$1
-#	str_orig=${str_orig//./}  # Remove .
-#	str_orig=${str_orig//:/}  # Remove :
-#	str_orig=${str_orig//@/}  # Remove @
-#	str_orig=${str_orig//[0-9]/}
-
-#	str_update=$2
-#	str_update=${str_update//./}  # Remove .
-#	str_update=${str_update//:/}  # Remove :
-#	str_update=${str_update//@/}  # Remove @
-#	str_update=${str_update//[0-9]/}
-
-
 
 	if [ $str_orig == $str_update ]
 		then
@@ -59,6 +95,80 @@ function updateTypeStringMatch {
 	fi
 
 }
+
+
+
+## Loop through all library updates
+
+
+
+
+
+# For each update
+
+# if added
+# 	check list of most recent commits
+	#	if simlar, then mark as update
+	#	If nothing simlilar, then mark as new
+# if removed
+	# Look at other dependencies in commit (adds only)
+	#	If similar, then update
+
+
+
+
+########
+
+
+### Work to make this more efficient
+function examineRow {
+	dcl_id=$1
+
+
+#	echo $dcl_id
+
+	action=`sqlite3 $db  "SELECT action FROM view_processedLibrarysauthors WHERE dcl_id='$dcl_id';"`
+	echo $action
+
+#	if [ $action eq "added" ]
+	if [ "$action" == "added" ]
+	then
+		echo "check added"  # Same
+	else
+
+
+
+
+		echo "check removed" # Diff
+	fi
+
+
+
+
+
+
+#	echo examine
+}
+
+
+
+
+
+### Loop through all items in the db
+sqlite3 $db "select dcl_ID from view_processedLibrarysauthors where appID =3 limit 1" | while read dcl_id; do
+    # use $theme_name and $guid variables
+
+## Todo: Pass in the needed variables
+
+
+examineRow $dcl_id
+#    echo "theme: $name, guid: $action"
+#    echo "________________"
+done
+
+
+
+
 
 
 ## Loop through all of the updates to determine the name of the dependency and app to see if the new dependcy is like the old one
@@ -99,12 +209,12 @@ function updateTypeStringMatch {
 ## Test the String matching function
 
 
-updateTypeStringMatch "com.anthonycr.grant:permissions:1.0" "com.anthonycr.grant:permissions:1.1.2"
-updateTypeStringMatch "com.google.dagger:dagger:2.0.1" "com.google.dagger:dagger:2.0.1"
-updateTypeStringMatch "net.i2p.android:client:0.6@aar" "net.i2p.android:client:0.7" ## will cause problems
+#updateTypeStringMatch "com.anthonycr.grant:permissions:1.0" "com.anthonycr.grant:permissions:1.1.2"
+#updateTypeStringMatch "com.google.dagger:dagger:2.0.1" "com.google.dagger:dagger:2.0.1"
+#updateTypeStringMatch "net.i2p.android:client:0.6@aar" "net.i2p.android:client:0.7" ## will cause problems
 
-updateTypeStringMatch "org.jsoup:jsoup:1.9.1" "org.jsoup:jsoup:1.9.2"
-updateTypeStringMatch "com.squareup:otto:1.3.8" "libs/jsoup-1.8.1.jar"
+#updateTypeStringMatch "org.jsoup:jsoup:1.9.1" "org.jsoup:jsoup:1.9.2"
+#updateTypeStringMatch "com.squareup:otto:1.3.8" "libs/jsoup-1.8.1.jar"
 
 
 ## Remove all numbers
